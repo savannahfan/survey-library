73968ec45e0fd38bf290d35641a1bcaad638df6f
savannahfan <71484163+savannahfan@users.noreply.github.com>
2021-07-13 01:41:24 +0800

    Update index_homepage.js

--- examples/jquery/index_homepage.js
+++ examples/jquery/index_homepage.js
@@ -1,3 +1,20 @@
+function GetRequest() {
+	var url = location.search;
+    var theRequest = new Object();
+	if (url.indexOf("?") != -1) {
+		var str = url.substr(1);
+        strs = str.split("&");
+        for (var i = 0; i < strs.length; i++) {
+			theRequest[strs[i].split("=")[0]] = decodeURI(strs[i].split("=")[1]);
+        }
+	}
+    return theRequest;
+    }
+var con = GetRequest();
+alert(con.eraderId+ " and " + con.bookId)
+
+
+
 function init() {
   //Add the price property into choices
   Survey.Serializer.addProperty("itemvalue", "price:number");
