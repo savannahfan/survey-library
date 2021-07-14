379bdad64bba1a506a756f5380a08afb54081918
savannahfan <71484163+savannahfan@users.noreply.github.com>
2021-07-14 19:12:52 +0800

    Update SWAS.js

--- examples/jquery/SWAS.js
+++ examples/jquery/SWAS.js
@@ -2,6 +2,7 @@ function init() {
   //Add the price property into choices
   Survey.Serializer.addProperty("itemvalue", "price:number");
 
+  var eraderId=Cookies.get("eraderId");
 
 
   var modifyCommentQuestion=function modifyCommentQuestion(value,question) {
@@ -34,6 +35,12 @@ function init() {
     pages: [
       {
           questions: [
+            {
+              name: "E-Reader ID",
+              type: "text",
+              title: "E-Reader ID",
+              visible:false
+            },
             {
               type: "rating",
               name: "transportation1",
@@ -382,6 +389,10 @@ function init() {
 
 
   survey.onComplete.add(function(result) {
+
+    var ereaderQues=survey.getQuestionByName("E-Reader ID");
+    ereaderQues.value=eraderId;
+
     console.log(JSON.stringify(survey.data));
 
     var resultData = survey.data;
@@ -404,14 +415,7 @@ function init() {
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
 
-    xhr.onload = function() {
-      if (this.status == 200) {
-          alert(this.response);
-        }
-      else {
-        alert('1111'+this.status);
-      }
-    }
+    
     xhr.send(JSON.stringify(resultData));
     console.log(JSON.stringify(resultData))
 
